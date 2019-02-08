583b224a17f80de1c667dacbcf41cfb357ad9224
Aoba-chan <35652338+Aoba-chan@users.noreply.github.com>
2019-02-08 00:36:05 -0700

    Making changes to test file

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Hello {
  public static void main(String[] args ) {
    System.out.println("Hello there, how are you doing?");
+   System.out.println("What is your name?");
+   Scanner reader = new Scanner(System.in);
+   String name = reader.nextLine();
+   System.out.println("Hello " + name);
  }
 }
