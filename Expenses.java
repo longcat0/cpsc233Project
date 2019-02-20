88f13e34cf6a9cef6136d0de10213f7dfe16b21e
Aron Mao <aron.mao@ucalgary.ca>
2019-02-20 16:45:11 -0700

    Complete working version of the Expenses class. May have some bugs tho.

--- Expenses.java
+++ Expenses.java
@@ -26,6 +26,14 @@ class Expenses {
     this.balance = object.balance;
   }
 
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
 
   public void setName(String someName) {
     this.name = someName;
@@ -36,7 +44,7 @@ class Expenses {
       this.balance += amount;
 
     } else {
-      System.out.println("Removal amount is negative or it causes balance to be negative.");
+      System.out.println("Input is a negative number. Please try again.");
     }
   }
 
@@ -55,7 +63,8 @@ class Expenses {
   public void printInfo() {
     makeLines();
     System.out.println();
-    System.out.printf(("Expense Name: %s \nBalance: %f"), this.name, this.balance);
+    System.out.println("Expense Name: " + this.getName());
+    System.out.println("Balance: " + this.getBalance());
     System.out.println();
   }
 
@@ -72,139 +81,158 @@ class Expenses {
     System.out.println("ListExpenses - Lists out all the expenses that the user has created");
   }
 
+  public static void runExpenseProgram() {
+    makeLines();
+    System.out.println();
+    System.out.println("Welcome! This program will help you keep track of your everyday spending.\nFor a list of commands, please type \"Commands\" into the prompt. Note: Commands are case sensitive." );
+    System.out.println();
+    makeLines();
+
+    HashMap<String, Expenses> all = new HashMap<String, Expenses>();
+
+    Scanner keyboard = new Scanner(System.in);
+
+    boolean status = true;
+
+    while (status) {
+      System.out.println();
+      System.out.print("What would you like to do? ");
+      String command = keyboard.nextLine();
+      System.out.println();
+
+      switch(command) {
+
+        case "Commands":
+         printCommands();
+         break;
+
+
+       case "Quit":
+         System.out.println("See you soon. Have a nice day :)");
+         status = false;
+         break;
+
+
+       case "CalculateGST":
+         int x = 1;
+         double amount = 0;
+
+         do {
+           try {
+             System.out.println();
+             System.out.print("What is the amount? ");
+             amount = Double.parseDouble(keyboard.nextLine());
+             x = 0;
+           } catch (Exception e) {
+             System.out.println("Your input is invalid. Please try again.");
+           }
+         } while (x == 1);
+
+         System.out.println("Your total with GST is: " + calculateGST(amount));
+         break;
+
+
+      case "CreateExpense":
+         System.out.print("Enter a name: ");
+         String name = keyboard.nextLine();
+         all.put(name, new Expenses(name));
+         break;
+
+
+     case "ListExpenses":
+         System.out.print("Your current number of expenses: " + Expenses.numberOfAccounts + "\n");
+         for (String key : all.keySet()) {
+          all.get(key).printInfo();
+         }
+         break;
+
+
+     case "AddToExpense":
+         int y = 1;
+         double amount2 = 0;
+         int count = 0;
+
+         do {
+           System.out.println();
+           System.out.print("Which expense would you like to add to? ");
+           String name2 = keyboard.nextLine();
+           if (all.containsKey(name2)) {
+
+             do {
+               try {
+                 System.out.println();
+                 System.out.print("How much do you want to add? ");
+                 amount2 = Double.parseDouble(keyboard.nextLine());
+                 y = 0;
+               } catch (Exception e) {
+                 System.out.println("Your input is invalid. Please try again.");
+               }
+             } while (y == 1);
+
+             all.get(name2).addBalance(Math.round(amount2 * 100) / 100.0);
+             if (amount2 >= 0) {
+               System.out.println("Added.");
+               count = 1;
+             }
+
+           } else {
+             System.out.println();
+             System.out.println("Does not exist. Please try again.");
+           }
+
+         } while (count == 0);
+         break;
+
+     case "RemoveFromExpense":
+         int z = 1;
+         double amount3 = 0;
+         int count2 = 0;
+
+         do {
+           System.out.println();
+           System.out.print("Which expense would you like to remove from? ");
+           String name3 = keyboard.nextLine();
+           if (all.containsKey(name3)) {
+
+             do {
+               try {
+                 System.out.println();
+                 System.out.print("How much do you want to remove? ");
+                 amount3 = Double.parseDouble(keyboard.nextLine());
+                 z = 0;
+               } catch (Exception e) {
+                 System.out.println("Your input is invalid. Please try again.");
+               }
+             } while (z == 1);
+
+             if ((amount3 >= 0) && ((all.get(name3).getBalance() - amount3) >= 0)) {
+               all.get(name3).removeAmount(Math.round(amount3 * 100) / 100.0);
+               System.out.println("Removed.");
+               count2 = 1;
+             } else {
+               System.out.println("Removal amount is negative or it causes balance to be negative. Please try again.");
+             }
+
+           } else {
+             System.out.println();
+             System.out.println("Does not exist. Please try again.");
+           }
+
+         } while (count2 == 0);
+             break;
+
+     default:
+       System.out.println("Unknown command. Please try again.");
+       }
+     }
+
+  }
+
 
 
    public static void main(String[] args) {
 
-     makeLines();
-     System.out.println();
-     System.out.println("Welcome! For a list of commands, please type \"Commands\" into the prompt. Note: Commands are case sensitive." );
-     System.out.println();
-     makeLines();
-
-     HashMap<String, Expenses> all = new HashMap<String, Expenses>();
-
-     Scanner keyboard = new Scanner(System.in);
-
-     boolean status = true;
-
-     while (status) {
-       System.out.println();
-       System.out.print("What would you like to do? ");
-       String command = keyboard.nextLine();
-       System.out.println();
-
-       switch(command) {
-
-         case "Commands":
-          printCommands();
-          break;
-
-
-        case "Quit":
-          System.out.println("See you soon. Have a nice day :)");
-          status = false;
-          break;
-
-
-        case "CalculateGST":
-          int x = 1;
-          double amount = 0;
-
-          do {
-            try {
-              System.out.println();
-              System.out.print("What is the amount? ");
-              amount = Double.parseDouble(keyboard.nextLine());
-              x = 0;
-            } catch (Exception e) {
-              System.out.println("Your input is invalid. Please try again.");
-            }
-          } while (x == 1);
-
-          System.out.println("Your total with GST is: " + calculateGST(amount));
-          break;
-
-
-       case "CreateExpense":
-          System.out.print("Enter a name: ");
-          String name = keyboard.nextLine();
-          all.put(name, new Expenses(name));
-          break;
-
-
-      case "ListExpenses":
-          System.out.print("Your current number of expenses: " + Expenses.numberOfAccounts + "\n");
-          for (String key : all.keySet()) {
-           all.get(key).printInfo();
-           System.out.println();
-          }
-          break;
-
-
-      case "AddToExpense":
-          System.out.print("Which expense would you like to add to? ");
-          String name2 = keyboard.nextLine();
-          int y = 1;
-          double amount2 = 0;
-          int count = 0;
-
-          do {
-            if (all.containsKey(name2)) {
-              do {
-                try {
-                  System.out.println();
-                  System.out.println("How much do you want to add? ");
-                  amount2 = Double.parseDouble(keyboard.nextLine());
-                  y = 0;
-                } catch (Exception e) {
-                  System.out.println("Your input is invalid. Please try again.");
-                }
-              } while (y == 1);
-              all.get(name2).addBalance(amount2);
-              System.out.println("Added.");
-              count = 1;
-            } else {
-              System.out.println("Does not exist. Please try again.");
-            }
-
-          } while (count == 0);
-          break;
-
-      case "RemoveFromExpense":
-        ;
-
-      default:
-        System.out.println("Unknown command. Please try again.");
-        }
-      }
-
-// This isn't part of the actual program. This is used to test to see if everyting works.
-     Expenses object = new Expenses("hello", 1239.79);
-     System.out.println(object.name);
-     System.out.println(object.balance);
-     System.out.println(object.numberOfAccounts);
-     object.printInfo();
-
-
-
-     try {
-       object.removeAmount(1239.0);
-       object.addBalance(12321);
-
-     } catch (Exception e) {
-       System.out.println(e);
-
-     } finally {
-      System.out.println("Testing...");
-    }
-
-
-
-     all.get("Hello").printInfo();
-
-
-
+    runExpenseProgram();
 
    }
 }
