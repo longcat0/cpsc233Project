e191d9089722b60a9e8c5246aa462fa6797b865b
MustafaMM <47229509+MustafaMM@users.noreply.github.com>
2019-03-10 14:48:52 -0600

    The GUI for demo 2.

---
+++ Edited/Expense.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+class Expense {
+  ArrayList<String> transactions = new ArrayList<String>();
+
+  public Expense() {
+
+  }
+
+  private static String makeLines(){
+    String lines = ("-------------");
+    return lines;
+  }
+  public void putIntoExpense(String name, double amount) {
+    Date date = new Date();
+    String info = ( "\n" + makeLines() + "\nTime and Date: " + date.toString() + "\nBudget: " + name + "\n" + "Amount: " + amount);
+    transactions.add(info);
+  }
+  public void printInfo() {
+    for (int i = 0; i < transactions.size(); i++) {
+      System.out.print(transactions.get(i));
+    }
+  }
+
+}
---
+++ Edited/Income1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Income1 {
+
+  private double amount = 0.0;
+
+  public Income1() {
+
+  }
+
+  public double getAmount() {
+    return this.amount;
+  }
+
+  public void setAmount(double amount) {
+    this.amount = (amount > 0) ? (amount) : 0;
+  }
+
+  public void inputIncome() {
+    Scanner reader = new Scanner(System.in);
+    int temp = 0;
+    do {
+      System.out.println();
+      System.out.print("What is your income? Please enter a positive number: ");
+      try {
+       // double income = Double.parseDouble(reader.nextLine());
+        //setAmount(income);
+        temp = 1;
+      } catch (Exception e) {
+        System.out.println();
+        System.out.println("Invalid input. Please try again.");
+      }
+    } while (temp == 0);
+
+  }
+
+}
