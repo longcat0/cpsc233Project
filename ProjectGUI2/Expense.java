7fed72f0c2f7d1eb59fed88a36e07ef84037993f
Sharif <47335894+ehsanis@users.noreply.github.com>
2019-03-25 12:43:06 -0600

    Update Expense.java

--- ProjectGUI2/Expense.java
+++ ProjectGUI2/Expense.java
@@ -7,17 +7,35 @@ public class Expense {
   private double amount;
   private String time;
   private String currentAccount; // The account that the expense relates to
+  private Budgets b;
+  
 
   public Expense(String budgetName, double amount, String currentAccount) {
+	b = new Budgets(budgetName, amount);
     Date datto = new Date();
     this.budgetName = budgetName;
-    this.amount = amount;
+    setAmount(amount);
     this.time = datto.toString();
     this.currentAccount = currentAccount;
   }
 
   public String getBudgetName() {
     return budgetName;
+  }
+  public void setAmount(double amount)
+  {
+	  if (amount < 0)
+	  {
+		  this.amount = this.amount;
+	  }
+	  else if (amount > b.getBudgAmount())
+	  {
+		  this.amount = this.amount;
+	  }
+	  else 
+	  {
+		  this.amount = amount;
+	  }
   }
 
   public double getAmount() {
